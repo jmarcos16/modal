eba87f0551b320b2fa0d2549c3f8e3548c4a5982
Zé Marcos <contato.marcosrf@gmail.com>
2023-10-20 11:44:25 -0300

    refactory modal

--- resources/views/components/main-modal.blade.php
+++ resources/views/components/main-modal.blade.php
@@ -1,16 +1,12 @@
 <div x-data="{
-    show: true,
     @if($attributes->wire('model')->value())
         show: @entangle($attributes->wire('model')->value()),
     @endif
     }"
-    {{-- Show modal if $name --}}
     x-on:open-modal.window="show =($event.detail.name === {{$name}})"
     x-on:close-modal.window="show !($event.detail.name === {{$name}})"
-    {{-- Close modal if click outside --}}
     x-on:click.away="show = false"
     x-on:keydown.escape.window="show = false"
-    {{-- Transitions modal --}}
     x-transition:enter="ease-out duration-300"
     x-transition:enter-start="opacity-0"
     x-transition:enter-end="opacity-100"
@@ -18,12 +14,9 @@
     x-transition:leave-start="opacity-100"
     x-transition:leave-end="opacity-0"
     x-show="show"
-    {{-- Style default --}}
     class="fixed inset-0 z-20 p-4 everflow"
     style="display: none;"
     >
-
-    {{-- background blur --}}
     <div 
         x-on:click="show = false"  
         class="absolute inset-0 overflow-hidden transition-opacity bg-gray-400 bg-opacity-75"
@@ -36,29 +29,21 @@
         aria-hidden="true">
     </div>
 
-    {{-- Modal body --}}
     <div class="flex flex-col w-full max-w-2xl mx-auto transform bg-white rounded shadow"> 
         <div class="flex items-center justify-between px-5 py-3 border-b">
-            <div>Title Modal</div>
+            <div>{{$title}}</div>
             <button x-on:click="show = false" class="text-2xl"> 
                 &times;
             </button>
         </div>
 
         <div class="p-5">
-            My body
+            {{$slot}}
         </div>
 
-        <div class="flex items-center justify-end gap-2 p-3 border-t">
-            <button class="px-4 py-2 text-white bg-indigo-500 rounded hover:bg-indigo-600">Save</button>
-            {{-- <button class="px-4 py-2 text-red-600 bg-gray-200 border rounded">Cancel</button> --}}
-            {{-- close button --}}
-            <button 
-                x-on:click="show = false"
-                class="px-4 py-2 text-red-600 bg-gray-200 border rounded">Cancel</button>
-            </button>
-
-        </div>
+        @if ($footer)
+            {{$footer}}
+        @endif
 
     </div>
 
--- resources/views/livewire/example.blade.php
+++ resources/views/livewire/example.blade.php
@@ -3,7 +3,7 @@
 use Livewire\Volt\Component;
 
 new class extends Component {
-    public $modal = true;
+    public $modal = false;
 }; ?>
 
 <div>
@@ -11,5 +11,16 @@ new class extends Component {
         class="px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-600" 
         wire:click="$toggle('modal')">Open Modal</button>
 
-        <x-main-modal name="open-modal" wire:model="modal"></x-main-modal>
+        <x-main-modal name="open-modal" wire:model="modal" title="My Modal Test">
+            <div class="px-5 py-3">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus laboriosam quis placeat aspernatur natus. Ipsum enim, nemo quaerat animi eius ullam eos ratione fuga perferendis aspernatur quas amet, dicta tempora.
+            </div>
+
+            <x-slot name="footer">
+                <div class="px-5 py-3 border-t">
+                    <x-primary-button>Save</x-primary-button>
+                    <x-secondary-button wire:click="$toggle('modal')">Close</x-secondary-button>
+                </div>
+            </x-slot>
+        </x-main-modal>
 </div>
